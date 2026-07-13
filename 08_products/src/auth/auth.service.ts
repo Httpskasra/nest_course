6856e8af9e3c17f6f46fe1dd344ef77e0b89b2be
kasra <kasrarahmanian3@gmail.com>
2026-07-13 19:19:32 +0330

    finish register and login

--- 08_products/src/auth/auth.service.ts
+++ 08_products/src/auth/auth.service.ts
@@ -1,8 +1,12 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { RegisterDto } from './dto/Register.dto';
 import { LoginDto } from './dto/Login.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
@@ -13,8 +17,9 @@ export class AuthService {
     if (existUser) {
       throw new ConflictException('ایمیل قبلا وجود هست');
     }
+    const passworHash = await bcrypt.hash(dto.password, 12);
     return this.prisma.user.create({
-      data: dto,
+      data: { ...dto, password: passworHash },
       select: {
         id: true,
         name: true,
@@ -24,5 +29,18 @@ export class AuthService {
       },
     });
   }
-  async login(dto: LoginDto) {}
+  async login(dto: LoginDto) {
+    const user = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+    if (!user) {
+      throw new UnauthorizedException('ورود نامعتبر');
+    }
+
+    const isValid = await bcrypt.compare(dto.password, user.password);
+    if (!isValid) {
+      throw new UnauthorizedException('رمز نامعتبر');
+    }
+    return true;
+  }
 }
