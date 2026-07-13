0f5d418726b1f5331419fc7ea1c3cdb8ef94710a
kasra <kasrarahmanian3@gmail.com>
2026-07-13 19:44:03 +0330

    add strategy

---
+++ 08_products/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,39 @@
+
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+export interface JWTPayload{
+    sub:string,
+    email:string,
+}
+
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+
+    const secret=process.env.JWT_SECRET
+
+    if(!secret){
+        throw new Error("JWT_SECRET not set in env")
+    }
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: secret,
+    });
+  }
+
+  async validate(payload: JWTPayload) :Promise<JWTPayload>{
+
+    if(!payload.sub  || !payload.email )
+        throw new UnauthorizedException("توکن نامعتبر است")
+    return {
+    sub:payload.sub,
+    email:payload.email,
+    }
+}
+
+}
